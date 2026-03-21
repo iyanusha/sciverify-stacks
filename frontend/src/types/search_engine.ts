a383339443724d4e7f5bfd813970daad4a1d1602
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-21 11:18:28 +0100

    Add publication search engine - commit 19/50

--- frontend/src/types/search_engine.ts
+++ frontend/src/types/search_engine.ts
@@ -1,2 +1,4 @@
 
 export interface Search_engineD8 { id: string; name: string; value: number; ts: number; }
+
+export interface Search_engineD18 { id: string; name: string; value: number; ts: number; }
